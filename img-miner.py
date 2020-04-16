3e6418071704cffc9b00ed2fd259dd844d2b4687
David Botelho Mariano <48680041+david-botelho-mariano@users.noreply.github.com>
2020-04-16 17:10:51 -0300

    Update img-miner.py

--- img-miner.py
+++ img-miner.py
@@ -1,5 +1,4 @@
 import time
-import random
 import urllib
 
 from selenium import webdriver
@@ -9,49 +8,38 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 chrome_opcoes = Options()
-#chrome_opcoes.add_argument("--headless")
 chrome_opcoes.add_argument("--log-level=3")
 chrome_opcoes.binary_location = "chrome-windows/chrome.exe"
 
 chrome_navegador = webdriver.Chrome('chrome-windows/chrome-driver.exe', chrome_options=chrome_opcoes)
 chrome_navegador.set_window_size(1920,1080)
-chrome_navegador.get('https://www.google.com/search?q=cachorro&tbm=isch&ved=2ahUKEwiBjsTEs-zoAhWnBrkGHYlcAGYQ2-cCegQIABAA&oq=cachorro&gs_lcp=CgNpbWcQAzIECCMQJzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzoCCAA6BQgAEIMBUK4uWIA-YOs_aABwAHgAgAHLAYgByAqSAQUwLjcuMZgBAKABAaoBC2d3cy13aXotaW1n&sclient=img&ei=dQaYXsH_KKeN5OUPibmBsAY&bih=435&biw=992&client=firefox-b-d')
-#cachorro filetype:jpg
+chrome_navegador.get('https://www.google.com/search?q=iphone&client=firefox-b-d&sxsrf=ALeKk03BuTfeo8NcyPB3UnFgfW18GDi6rA:1587065328788&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiYiqGJ1-3oAhUUA9QKHZiVCaYQ_AUoA3oECA8QBQ&biw=992&bih=435')
 
-print('[+]esperando carregamento da pagina')
-time.sleep(10)
-
-contador_imagens = 1
-body_div = chrome_navegador.find_element_by_css_selector('body')
-
-while True:
-	body_div.send_keys(Keys.SPACE)
-	
-	for x in range(0, 4, 1):
-
-		if x == 3:
-			print("pausa")
-			time.sleep(5)
-
+def scrolToMaxBottom():
+	for x in range(0, qtd_imagens_desejadas/4, 1):
+		time.sleep(5)
+		body_div.send_keys(Keys.SPACE)
+		print("esperando quatro imagens da linha: " + str(x) + " serem carregadas")
 
 		try:
-			img = chrome_navegador.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[' + str(contador_imagens) +']/a[1]/div[1]/img')
+			chrome_navegador.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[5]/input").click()
+			print("sucesso ao pressionar o botao 'mostrar mais'")
+		except Exception as e:
+			pass
+	
+def minerarImagens():
+	for id_imagem in range(0, qtd_imagens_desejadas, 1):
+		try:
+			img = chrome_navegador.find_element_by_xpath('/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[' + str(id_imagem) +']/a[1]/div[1]/img')
 			src = img.get_attribute('src')
-			urllib.urlretrieve(src, str(contador_imagens) + ".png")
-			contador_imagens = contador_imagens + 1
-			print(contador_imagens)
+			urllib.urlretrieve(src, "imgs-mineradas/" + str(id_imagem) + ".png")
+			print("id da imagem da imagem minerada: " + str(id_imagem))
 				
 		except Exception as e:
-			print(e)
+			pass
 
-			try:
-				#pode ser uma botao de mostrar mais
-				chrome_navegador.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[5]/input").click()
-			except Exception as e:
-				#pode ser uma propaganda
-				contador_imagens = contador_imagens + 1
-				print('[+]propaganda detectada')
-			
-			
-
-print("[+]acabou-se")
+if __name__ == '__main__':
+	qtd_imagens_desejadas = 1000
+	body_div = chrome_navegador.find_element_by_css_selector('body')
+	scrolToMaxBottom()
+	minerarImagens()
